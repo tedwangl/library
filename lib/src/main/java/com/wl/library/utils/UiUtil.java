35f38889551c00daa92839599cfb8a676cf12bc0
wanglei <2541496842@qq.com>
2019-09-09 15:13:26 +0800

    init

--- lib/src/main/java/com/wl/library/utils/UiUtil.java
+++ lib/src/main/java/com/wl/library/utils/UiUtil.java
@@ -1,8 +1,13 @@
 package com.wl.library.utils;
 
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
 
 public class UiUtil {
+
+    private static final long SHOW_SPACE = 200L;
 
     public static void setText(String text, TextView textView) {
         if (textView != null) {
@@ -14,4 +19,27 @@ public class UiUtil {
         }
     }
 
+    /**
+     * 显示软键盘
+     */
+    public static void showSoftInput(final View view) {
+        if (view == null || view.getContext() == null) return;
+        final InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        view.requestFocus();
+        view.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                imm.showSoftInput(view, InputMethodManager.SHOW_FORCED);
+            }
+        }, SHOW_SPACE);
+    }
+
+    /**
+     * 隐藏软键盘
+     */
+    public static void hideSoftInput(View view) {
+        if (view == null || view.getContext() == null) return;
+        InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
 }
